aa9453d8631b65a30ff6acb736e8336870a83789
JEREMIA-SUNNY <you@example.com>
2022-04-08 01:37:55 +0530

    Initial commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return ( 
         <header className="navbar">
            <div className="navbar2"><img className="img1" src="https://indianmemetemplates.com/wp-content/uploads/troll-face-768x628.jpg" alt="" />
-            <h2 className="text-h2">Meme genarator</h2></div> 
+            <h2 className="text-h2">Meme generator</h2></div> 
             <p>Enjoy-trolling</p>
         </header>
      );
